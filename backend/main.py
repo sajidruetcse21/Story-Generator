e6227bd5f673a046b53c684f2134a221fc6293e7
MD.SAJEDUL ISLAM <m.sajid1495@gmail.com>
2026-01-26 18:09:48 +0600

    configured environment variable

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware 
+from core.config import settings
 
 
 app = FastAPI(
@@ -13,7 +14,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=settings.ALLOWED_ORIGINS, 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
